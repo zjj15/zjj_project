ae2878f99c1e25197be56b33f25ac3903dde266b
zhangjingjing <zhangjingjing@iauto.com>
2021-11-10 18:54:55 +0800

    modify

--- FileTest/FuncCoverage_01_Radio_Case00001.tcs/AbsPath.py
+++
@@ -1,7 +0,0 @@
-def getProjectPath() :
-    return __file__[:__file__.find("gaea") + len("gaea")]
-
-import sys
-if not (getProjectPath() in sys.path) :
-    sys.path.append(getProjectPath())
-print("init " + __file__)
