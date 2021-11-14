b96744b4a90659aee79e50f3fdb43d2e3021a0c8
zjj <2580043710@qq.com>
2021-11-14 22:52:53 +0800

    modify

--- Python/python_code/Exercise_9_Func.py
+++ Python/python_code/Exercise_9_Func.py
@@ -117,6 +117,103 @@ print('after discounts the price is : ',new_price)
 
 
 
+#global关键字
+count=5
+def Func6():
+    count=10
+    print(count)
+
+Func6()# 10
+print(count)#5
+
+
+num=5
+def Func7():
+    global num
+    num=10
+    print(num)
+
+Func7()#10
+print(num)#10
+
+
+
+#内嵌函数
+def MyFunc1():
+    print("MyFunc1")
+    def MyFunc2():
+        print('MyFunc2')
+    MyFunc2()
+
+MyFunc1()
+
+#闭包
+def MyFunc3(x):
+    def MyFunc4(y):
+        return x * y
+    return MyFunc4
+
+print(MyFunc3(5)(8))#40
+
+'''
+def MyFunc5():
+    x=5
+    def MyFunc6():函数值存放在栈里
+        x*=x#屏蔽外部x，使用局部未定义的x
+        return x
+    return MyFunc6()
+    #改进方法
+    '''
+
+
+def MyFunc5():
+    x=[5]
+    def MyFunc6():#函数值存放在栈里
+        x[0]*=x[0]#x[0]就是5
+        return x[0]
+    return MyFunc6()
+
+print(MyFunc5())
+
+
+def MyFunc5():
+    x=5
+    def MyFunc6():
+        nonlocal x#关键字意思是：不是局部变量
+        x*=x
+        return x
+    return MyFunc6()
+
+print(MyFunc5())
+
+#lambda表达式
+def func(x):
+    return 2*x+1
+
+#等同于
+lambda x:2*x+1
+g=lambda x:2*x+1
+print(g(5))# 
+
+lambda x,y:x+y
+g=lambda x,y:x+y
+print(g(3,4)) 
+
+#过滤器 filter(函数名，iterable)
+def func2(x):
+    return x%2
+
+temp=range(10)
+show=filter(func2, temp)#对象
+print(list(show))#[1, 3, 5, 7, 9]
+
+show=list(filter(lambda x:x%2, range(10)))
+print(show)#[1, 3, 5, 7, 9]
+
+#map 映射 map(函数名，iterable),把第二个参数里每一个值带入到第一个函数里运算并返回
+show=list(map(lambda x:x%2,range(10)))
+print(show)#[0, 1, 0, 1, 0, 1, 0, 1, 0, 1]
+
 
     
 
