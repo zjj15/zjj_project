bb104cf843de2bb2f127e45a3bb2418c056ce871
zhangjingjing <zhangjingjing@iauto.com>
2021-12-01 14:37:56 +0800

    modify

--- Python/python_code/Exercise_Test.py
+++ Python/python_code/Exercise_Test.py
@@ -1,8 +1,12 @@
 
-import time
-
-time1=int(time.time())
-while int(time.time())-time1 <5:
-    print(int(time.time()))
-    print(int(time1))
-    print(int(time.time()-time1))
+class Parent:        # 定义父类
+   def myMethod(self):
+      print ('调用父类方法')
+ 
+class Child(Parent): # 定义子类
+   def myMethod(self):
+      print ('调用子类方法')
+ 
+c = Child()          # 子类实例
+c.myMethod()         # 子类调用重写方法
+super(Child,c).myMethod()
