d90921c87fef198b1084599305362786482369ea
zhangjingjing <zhangjingjing@iauto.com>
2021-12-02 11:26:51 +0800

    modify

--- Python/python_code/Exercise_Test.py
+++ Python/python_code/Exercise_Test.py
@@ -10,3 +10,8 @@ class Child(Parent): # 定义子类
 c = Child()          # 子类实例
 c.myMethod()         # 子类调用重写方法
 super(Child,c).myMethod()
+
+#导入单个类；从一个模块中导入多个类
+#有一个car.py的脚本里面写了Car类、ElectricCar类
+#from car import Car
+#from car import Car, ElectricCar
